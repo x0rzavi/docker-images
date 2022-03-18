0945826a05dbf9982b47122a6439d47fa43fa66d
Avishek Sen <x0rzavi@gmail.com>
2022-03-19 00:06:49 +0530

    tag

--- build.sh
+++ build.sh
@@ -18,8 +18,8 @@ verbosity () {
 kernel_prepare () {
     cd /usr/src/linux
     make -j$(nproc) mrproper
-    #cp CONFIGS/xanmod/gcc/config .config
-    wget -O .config https://raw.githubusercontent.com/x0rzavi/gentoo-bits/main/config-5.16.14-gentoo-x0rzavi
+    cp CONFIGS/xanmod/gcc/config .config
+    #wget -O .config https://raw.githubusercontent.com/x0rzavi/gentoo-bits/main/config-5.16.14-gentoo-x0rzavi
     make -j$(nproc) olddefconfig
     verbosity "KERNEL PREPARATION COMPLETED SUCCESSFULLY"
 }
@@ -35,7 +35,7 @@ kernel_package () {
 }
 
 kernel_tag () {
-    version=$(grep 'Linux/x86' /usr/src/linux/.config | sed 's/# Linux\/x86 //;s/ Kernel Configuration//')
+    version=$(grep 'Linux/x86' /usr/src/linux/.config | sed 's/# Linux\/x86 /Xanmod-/;s/ Kernel Configuration//')
     seconds=$(stat -c '%X' /usr/src/linux/.config)
     tag="$version-$seconds"
     echo $tag > $workdir/release_tag
