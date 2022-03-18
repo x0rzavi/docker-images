7d02372ece71b2966e6e726fc0cb79105aeba789
Avishek <x0rzavi@gmail.com>
2022-03-18 12:16:02 +0530

    tag kernel

--- build.sh
+++ build.sh
@@ -41,8 +41,16 @@ kernel_package () {
     echo -e "\nKernel Packing Completed Successfully\n"
 }
 
+kernel_tag () {
+    version=$(grep 'Linux/x86' /usr/src/linux/.config | sed 's/# Linux\/x86 /Xanmod-/;s/ Kernel Configuration//')
+    seconds=$(stat -c '%W' /usr/src/linux/.config)
+    tag="$version-$seconds"
+    export KERNEL_TAG="$tag"
+}
+
 deps_setup
 package_setup
 kernel_prepare
 kernel_build
 kernel_package
+kernel_tag
