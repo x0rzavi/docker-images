0a5b409b1022e21c4e8887e9be5206e72d008342
Avishek Sen <x0rzavi@gmail.com>
2022-03-19 20:40:21 +0530

    finalize

--- upload.sh
+++ upload.sh
@@ -16,7 +16,7 @@ verbosity () {
 
 kernel_upload () {
     release_tag=$(cat $workdir/release_tag)
-    curl --progress-bar -T /usr/src/linux/linux.7z "https://oshi.at/$release_tag.7z?expire=43200" | sed -n '3p' | sed 's/ \[Download\]//' > /usr/src/linux/download_link.txt
+    curl --no-progress-meter -T /usr/src/linux/linux.7z "https://oshi.at/$release_tag.7z?expire=43200" | sed -n '3p' | sed 's/ \[Download\]//' > /usr/src/linux/download_link.txt
     download_link=$(cat /usr/src/linux/download_link.txt)
     verbosity $download_link
 }
