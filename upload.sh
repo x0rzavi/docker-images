cbbfbc06b837bf5d643ab6bfb93d4915250edb95
Avishek Sen <x0rzavi@gmail.com>
2022-03-19 19:24:00 +0530

    test temp upload

---
+++ upload.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Exit mechanism
+trap "exit" INT
+set -e
+
+# Author: https://github.com/x0rzavi
+# Description: Temporarily upload built kernel archive to https://oshi.at/
+
+workdir=$(pwd)
+verbosity () {
+    echo -e "\n\n***********************************************"
+    echo -e "$1"
+    echo -e "***********************************************\n\n"
+}
+
+kernel_upload () {
+    release_tag=$(echo $workdir/release_tag)
+    curl --progress-bar -T /usr/src/linux/linux.7z "https://oshi.at/$release_tag.7z?expire=43200" | sed -n '3p' | sed 's/ \[Download\]//' > /usr/src/linux/download_link.txt
+    download_link=$(echo /usr/src/linux/download_link.txt)
+    verbosity $download_link
+}
